fd0599c3585144143ca07a49aa655c65a0005680
Shesh Ghimire <sheshgh@outlook.com>
2025-02-18 13:46:38 +0545

    add: support for type of item being imported.

--- Src/Traits/ImportResolver.php
+++ Src/Traits/ImportResolver.php
@@ -9,6 +9,9 @@ use Nette\PhpGenerator\PhpNamespace;
 trait ImportResolver {
 	private PhpNamespace $namespace;
 
+	/** @var array{0:string,1:string,2:string} */
+	private array $currentlyImportingItem;
+
 	public function setNamespace( PhpNamespace $namespace = null ): static {
 		$this->namespace ??= $namespace ?? new PhpNamespace( '' );
 
@@ -20,35 +23,54 @@ trait ImportResolver {
 	}
 
 	/**
-	 * @param string $item
+	 * @param PhpNamespace::NAME* $type
 	 * @return string The alias of the $item that is being imported. Mostly the classname only without the
 	 *                namespace part. If already has alias, alias is prefixed with last part of namespace.
 	 */
-	public function addUseStatementOf( string $item ): string {
-		$classnameOnly = $alias = Helpers::extractShortName( $item );
+	public function addUseStatementOf( string $item, string $type = PhpNamespace::NAME_NORMAL ): string {
+		$nameOnly                     = Helpers::extractShortName( $item );
+		$this->currentlyImportingItem = array( $item, $type, $nameOnly );
 
-		if ( $this->hasAlreadyImported( $item ) ) {
-			return $classnameOnly;
+		if ( $this->hasAlreadyImportedCurrentItem() ) {
+			return $this->currentItemResolvedAs( $nameOnly );
 		}
 
-		$lastPartOfNamespace = $this->hasAlreadyAliasedImportedItemAs( $classnameOnly )
-			? Helpers::extractShortName( Helpers::extractNamespace( $item ) )
-			: '';
+		$namePrefixedByLastPartOfNamespace = $this->prefixedWithLastPartOfCurrentItemNamespace();
 
-		$this->getNamespace()->addUse( $item, $alias = $lastPartOfNamespace . $classnameOnly );
+		$this->getNamespace()->addUse( $item, $namePrefixedByLastPartOfNamespace, of: $type );
 
-		return $alias;
+		return $this->currentItemResolvedAs( $namePrefixedByLastPartOfNamespace );
 	}
 
-	private function hasAlreadyImported( string $item ): bool {
+	private function hasAlreadyImportedCurrentItem(): bool {
+		[$item] = $this->currentlyImportingItem;
+
 		return in_array( $item, haystack: $this->getNamespace()->getUses(), strict: true );
 	}
 
-	private function hasAlreadyAliasedImportedItemAs( string $classnameOnly ): bool {
-		return ! empty( $uses = $this->getNamespace()->getUses() ) && array_filter(
+	private function hasAlreadyAliasedCurrentItem(): bool {
+		[, $type, $nameOnly] = $this->currentlyImportingItem;
+
+		return ! empty( $uses = $this->getNamespace()->getUses( of: $type ) ) && array_filter(
 			array: $uses,
-			callback: static fn( string $alias ): bool => $alias === $classnameOnly,
+			callback: static fn( string $alias ): bool => $alias === $nameOnly,
 			mode: ARRAY_FILTER_USE_KEY
 		);
+	}
+
+	private function prefixedWithLastPartOfCurrentItemNamespace(): string {
+		[$fqcn,, $nameOnly] = $this->currentlyImportingItem;
+
+		$namespaceLastPart = $this->hasAlreadyAliasedCurrentItem()
+			? Helpers::extractShortName( Helpers::extractNamespace( $fqcn ) )
+			: '';
+
+		return $namespaceLastPart . $nameOnly;
+	}
+
+	private function currentItemResolvedAs( string $item ): string {
+		unset( $this->currentlyImportingItem );
+
+		return $item;
 	}
 }
